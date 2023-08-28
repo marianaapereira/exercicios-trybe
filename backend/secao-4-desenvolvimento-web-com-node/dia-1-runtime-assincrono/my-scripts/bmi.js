11e1bef022d19eed6805c18442a64c8f172fc4ec
Mariana Pereira <marianapereira.s@live.com>
2023-08-28 19:16:41 -0300

    Exercício 3

--- backend/secao-4-desenvolvimento-web-com-node/dia-1-runtime-assincrono/my-scripts/bmi.js
+++ backend/secao-4-desenvolvimento-web-com-node/dia-1-runtime-assincrono/my-scripts/bmi.js
@@ -1,8 +1,15 @@
-function calculateBMI() {
-  const weight = 85;
-  const height = 1.7;
+const readline = require('readline-sync');
 
-  return weight / (height * height);
+function calculateBMI() {
+  // const weight = 85;
+  // const height = 1.7;
+
+  const weight = readline.questionFloat('What is your weight? ');
+  const height = readline.questionFloat('What is your height? ');
+
+  const bmi = weight / (height * height)
+
+  return Math.round(bmi * 10) / 10;
 }
 
 console.log(calculateBMI());
